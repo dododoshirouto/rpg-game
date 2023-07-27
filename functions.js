e84b369d5e39147c845218172cbfa6ec38aa95df
dododoshirouto <yfkf19961009@gmail.com>
2023-07-28 06:42:26 +0900

    Vector2機能増やした

--- functions.js
+++ functions.js
@@ -15,6 +15,80 @@ class Vector2 {
         this.x = x;
         this.y = y;
     }
+    
+    clone() { return new Vector2(this.x, this.y); }
+    add(vec) { this.x += vec.x; this.y += vec.y; return this; }
+    mul(n) { this.x *= n; this.y *= n; return this; }
+    inv() { return mul(-1); }
+    mag() { return Vector2.dist(this, Vector2.zero); }
+    
+    static dist(v1, v2) {
+        let dx = v2.x - v1.x;
+        let dy = v2.y - v1.y;
+        return Math.sqrt(dx * dx + dy * dy);
+    }
+    
+    /** 内積 */
+    static dot(v1, v2) {
+        return v1.x * v2.x + v1.y * v2.y;
+    }
+    
+    /** 外積 */
+    static cross(v1, v2) {
+        return v1.x * v2.y - v1.y * v2.x;
+    }
+    
+    /** ベクトルの正規化 */
+    normalize() {
+        let mag = this.mag();
+        if (mag > 0) {
+            this.x /= mag;
+            this.y /= mag;
+        }
+        return this;
+    }
+    
+    /** ベクトルの角度を取得する */
+    angle() {
+        return Math.atan2(this.y, this.x);
+    }
+    
+    /** ベクトルの回転 */
+    rotate(angle) {
+        let cos = Math.cos(angle);
+        let sin = Math.sin(angle);
+        let x = this.x * cos - this.y * sin;
+        let y = this.x * sin + this.y * cos;
+        this.x = x;
+        this.y = y;
+        return this;
+    }
+    
+    /** ベクトルの線形補間 */
+    static lerp(vec1, vec2, t) {
+        let x = vec1.x + (vec2.x - vec1.x) * t;
+        let y = vec1.y + (vec2.y - vec1.y) * t;
+        return new Vector2(x, y);
+    }
+    
+    /** 2つのベクトルがなす角度を取得する */
+    static angleBetween(v1, v2) {
+        let dot = Vector2.dot(v1, v2);
+        let mag1 = v1.magnitude();
+        let mag2 = v2.magnitude();
+        let cos = dot / (mag1 * mag2);
+        return Math.acos(cos);
+    }
+    
+    /** 2つのベクトルが等しいかどうかを判定する */
+    static equal(vec1, vec2) {
+        return Math.abs(vec1.x - vec2.x) < 0.000001 && Math.abs(vec1.y - vec2.y) < 0.000001;
+    }
+    
+    /** ベクトルを文字列に変換する */
+    toString() {
+        return "(" + this.x + ", " + this.y + ")";
+    }
 }
 
 class Time {
